735a800a769fa9a975c4e04506e1ab79fdc29d4b
lorenestauffer <lorene.stauffer@campus-centre.fr>
2025-02-06 09:30:50 +0100

    Inscription client OK

---
+++ endpoints.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const db= require("./db");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+
+/*
+    * Route : Lister les produits
+    * GET /api/produits
+ */
+router.get("/produit", (req, res) => {
+    db.query("SELECT * FROM produit", (err, result) => {
+      if(err) {
+          return res.status(500).json({message: "Erreur du serveur"});
+      }
+      res.json(result);
+    });
+});
+
+/*
+    * Route : Récupérer un produit par son ID
+    * GET /api/produit/:id
+    * Exemple : GET /api/produits/3
+ */
+router.get("/produit/:id", (req, res) => {
+    const { id } = req.params; //const id = req.params.id (la ligne commenté équivaut à la ligne non commenté)
+
+    db.query("SELECT * FROM produit WHERE Id_produit = ?", [id], (err, result) => {
+        if(err) {
+            return res.status(500).json({message: "Erreur du serveur"});
+        }
+
+        if(result.length === 0) {
+            return res.status(404).json({message: "Produit non trouvé"});
+        }
+        res.json(result[0]); // Retournera uniquement le 1er résultat
+    });
+});
+
+
+/*
+    * Route : Inscription d'un client
+    * POST /api/client/register
+    * Exemple : JSON
+    * {
+    * "nom": "Dupont",
+    * "prenom": "Jean"
+    * "email": "jean.dupont@email.com"
+    * "mot de passe": "monMotDePasse"
+    * }
+ */
+router.post("/client/register", (req, res) => {
+ //const { nom, prenom, email, mot_de_passe } = req.body; // /!\/!\/!\ à reprendre avec les éléments de ma base de données sur la ligne en dessous!!
+    const { Nom_client, Prenom_client, Telephone_client, Mail_client, Mdp_client, Date_inscription, Adresse_client } = req.body; // /!\/!\/!\ à reprendre avec les éléments de ma base de données !!
+    // Contrôler si le mail est déjà présent dans la base de donnée
+    db.query("SELECT * FROM client WHERE Mail_client = ?", [Mail_client], (err, result) => {
+        if(err) {
+            return res.status(500).json({message: "Erreur du serveur"});
+        }
+
+        if(result.length > 0) {
+            return res
+                .status(400)
+                .json({message: "Cette adresse mail est déjà utilisé"});
+        }
+});
+
+   // Hachage du mdp
+   bcrypt.hash(	Mdp_client, 10, (err, hash) => {
+       if(err) {
+           return res
+               .status(500)
+               .json({message: "Erreur lors du hachage du mdp"});
+       }
+
+       // Insertion du nouveau client
+       db.query(
+           "INSERT INTO client (Nom_client, Prenom_client, Telephone_client, Mail_client, Mdp_client, Date_inscription, Adresse_client) VALUES (?,?,?,?,?,?,?)",
+           [Nom_client, Prenom_client, Telephone_client, Mail_client, hash, Date_inscription, Adresse_client],
+           (err, result) => {
+               if (err) {
+                   return res
+                       .status(500)
+                       .json({message: "Erreur lors du hachage du mdp"});
+               }
+
+               res
+                   .status(201)
+                   .json({message: "Inscription réussie", client_id: result.insertId});
+           },
+           );
+   });
+});
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 // Utilisation des routes
-// app.use("/api", routes);
+app.use("/api", routes);
 
 // Démarrer le serveur
 const PORT = process.env.PORT || 3000;
@@ -16,3 +16,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`L'API CafThé est démarré sur http://localhost:${PORT}`);
 });
+
